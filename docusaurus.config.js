ab9c08aec8611e682d4c32ec9eb4b6c283b53a4d
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-05-03 12:16:42 +0100

    feat: hero update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,7 +164,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/paths/intro',
               },
             ],
           },
