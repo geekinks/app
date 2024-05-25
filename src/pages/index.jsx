16cbc3da1e05cd18cdb37acfca9edbe6854bffd9
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-05-25 23:11:21 +0100

    feat: adjustment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,32 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Partners from '@site/src/components/Partners';
-import Testimonials from '../components/Testimonials';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Partners from "@site/src/components/Partners";
+import Testimonials from "../components/Testimonials";
 // import Pricing from '@site/src/components/Pricing';
-import FAQ from '@site/src/components/FAQ'
-import Courses from '@site/src/components/Courses';
-import Hero from '@site/src/components/Hero';
-import CustomDivider from '@site/src/components/CustomDivider';
-import styles from './index.module.css';
-
+import FAQ from "@site/src/components/FAQ";
+import Courses from "@site/src/components/Courses";
+import Hero from "@site/src/components/Hero";
+import Homepage from "@site/src/components/Home";
+import CustomDivider from "@site/src/components/CustomDivider";
+import styles from "./index.module.css";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`home`}
-      description="Description will go into a meta tag in <head />">
-      <Hero/>
+      description="Description will go into a meta tag in <head />"
+    >
+      <Hero />
+      {/* <Homepage/> */}
       <main>
-      <CustomDivider
-        label="Testimonials from Our Students" />
+        <CustomDivider label="Testimonials from Our Students" />
         <Testimonials />
-        <Courses/>
-        <CustomDivider
-        label="Our Partners" />
+        <Courses />
+        <CustomDivider label="Our Partners" />
         <Partners />
         {/* <Pricing/> */}
-        <FAQ/>
+        <FAQ />
       </main>
     </Layout>
   );
