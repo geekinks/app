0e2ee61a4a130692710cf9320e939ea0352598b7
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-06-05 15:30:01 +0100

    feat: UI hero page

---
+++ src/pages/service/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+function Service() {
+  return (
+    <Layout
+      title={`service`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <h1>service</h1>
+    </Layout>
+  );
+}
+
+export default Service;
---
+++ src/pages/writting/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "@theme/Layout";
+function Writting() {
+  return (
+    <Layout
+      title={`writting`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <h1>writting</h1>
+    </Layout>
+  );
+}
+
+export default Writting;
